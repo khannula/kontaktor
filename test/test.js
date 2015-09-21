4065f0d1b38334ddbc0099115850e48b14f312e1
Kari H <kartahan@gmail.com>
2015-09-21 12:02:55 +0300

    Some Mocha tests added
    Few test cases. Also Chai and Request modules added to package.json for
    easing testing.

--- test/test.js
+++ test/test.js
@@ -3,11 +3,43 @@
  */
 
 var assert = require("assert");
+var expect  = require("chai").expect;
+var request = require("request");
+var routes = require("../app/routes.js");
+
 describe('Array', function() {
     describe('#indexOf()', function () {
         it('should return -1 when the value is not present', function () {
             assert.equal(-1, [1,2,3].indexOf(5));
             assert.equal(-1, [1,2,3].indexOf(0));
+        });
+    });
+});
+
+describe('Kontaktor contacts', function() {
+    describe('http interface tests', function () {
+        var url = "http://localhost:8080/index.html";
+
+        it("GET returns status 200", function(done) {
+            request(url, function(error, response, body) {
+                expect(response.statusCode).to.equal(200);
+                done();
+            });
+        });
+        // stupid test and api should be changed
+        it("GET returns status 200 for *", function(done) {
+            url = "http://localhost:8080/notnot_index.html";
+            request(url, function(error, response, body) {
+                expect(response.statusCode).to.not.equal(404);
+                done();
+            });
+        });
+        it("DELETE returns status 200", function(done) {
+            url = "http://localhost:8080/api/todos/2";
+            request(url, function(error, response, body) {
+                expect(response.statusCode).to.equal(200);
+                done();
+            });
         });
     });
 });
